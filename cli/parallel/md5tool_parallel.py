b9de29e3e8c587cd135d5b32f7ce64c91afcbf89
Luis J. Villanueva <villanueval@si.edu>
2023-03-14 09:33:41 -0400

    Automating package install and no_workers

--- cli/parallel/md5tool_parallel.py
+++ cli/parallel/md5tool_parallel.py
@@ -6,7 +6,11 @@
 #
 # https://github.com/Smithsonian/MD5_tool/
 #
-# 19 Sep 2022
+# 14 Mar 2023
+#
+#  - New:
+#       * Automatically runs in all subfolders that have files
+#       * If no_workers is not given, set to (number of cores - 1)
 #
 # Digitization Program Office,
 # Office of the Chief Information Officer,
@@ -20,20 +24,35 @@ import sys
 import os
 import glob
 from functools import partial
-from pyfiglet import Figlet
-from pathlib import Path
+import subprocess
+
+# from pathlib import Path
 from time import localtime, strftime
 
+
+def check_package(package):
+    try:
+        __import__(package)
+    except ImportError:
+        p = subprocess.run(['python3', '-m', 'pip', 'install', '--user', '--upgrade', 'pip'])
+        p = subprocess.run(['python3', '-m', 'pip', 'install', '--user', package])
+
+
+check_package('pyfiglet')
+check_package('p_tqdm')
+
+
+from pyfiglet import Figlet
+
 # Parallel
-from multiprocessing import Pool
-from tqdm.auto import tqdm
+import multiprocessing
 from p_tqdm import p_map
 
 
 # Script variables
 script_title = "MD5 Tool - Parallel"
 subtitle = "Digitization Program Office\nOffice of the Chief Information Officer\nSmithsonian Institution\nhttps://dpo.si.edu"
-ver = "0.2.0"
+ver = "0.3.0"
 repo = "https://github.com/Smithsonian/MD5_tool/"
 lic = "Available under the Apache 2.0 License"
 
@@ -51,8 +70,18 @@ print("\n")
 print(f.renderText(script_title))
 print(msg_text.format(subtitle=subtitle, ver=ver, repo=repo, lic=lic))
 
-folder_md5 = sys.argv[1]
-no_workers = int(sys.argv[2])
+
+if len(sys.argv) == 3:
+    folder_md5 = sys.argv[1]
+    no_workers = int(sys.argv[2])
+    if multiprocessing.cpu_count() < no_workers:
+        no_workers = (multiprocessing.cpu_count()) - 1
+elif len(sys.argv) == 2:
+    folder_md5 = sys.argv[1]
+    no_workers = (multiprocessing.cpu_count()) - 1
+else:
+    sys.exit("Missing folder and/or no_workers arguments.\n Usage: ./md5toolparallel.py [FOLDER] [NO_WORKERS]")
+
 
 # Get current time
 current_time = strftime("%Y%m%d_%H%M%S", localtime())
@@ -67,15 +96,18 @@ def md5sum(filename):
     return "{} {}".format(d.hexdigest(), os.path.basename(filename))
 
 
-files = glob.glob("{}/*".format(folder_md5))
-
-if len(glob.glob("{}/*.md5".format(folder_md5))) > 0:
-    print("md5 file exists, exiting.")
-    sys.exit(9)
-else:
-    results = p_map(md5sum, files, **{"num_cpus": int(no_workers)})
-    with open("{}/{}_{}.md5".format(folder_md5, os.path.basename(os.path.dirname(folder_md5)), current_time), 'w') as fp:
-        fp.write('\n'.join(results))
+for root, dirs, files_indir in os.walk(folder_md5):
+    for folder_check in dirs:
+        print("\n Running on folder {} using {} workers".format(folder_check, no_workers))
+        if len(glob.glob("{}/{}/*.md5".format(root, folder_check))) > 0:
+            print("\n   md5 file exists, skipping...")
+            continue
+        files = glob.glob("{}/{}/*".format(root, folder_check))
+        if len(files) > 0:
+            results = p_map(md5sum, files, **{"num_cpus": int(no_workers)})
+            with open("{}/{}/{}_{}.md5".format(root, folder_check, folder_check, current_time),
+                      'w') as fp:
+                fp.write('\n'.join(results))
 
 
 sys.exit(0)
